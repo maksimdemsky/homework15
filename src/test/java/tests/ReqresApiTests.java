5b97f2c389ee50c790196016fa8d2126b54494e4
maksim <maksimdemsky@gmail.com>
2021-06-03 20:57:37 +0300

    user check test add

--- src/test/java/tests/ReqresApiTests.java
+++ src/test/java/tests/ReqresApiTests.java
@@ -3,6 +3,8 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class ReqresApiTests extends TestBase {
     @Test
@@ -12,5 +14,15 @@ public class ReqresApiTests extends TestBase {
                 .get("api/users/23")
                 .then()
                 .statusCode(404);
+    }
+    @Test
+    void singleUsersCheck() {
+        given()
+                .when()
+                .get("api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"));
     }
 }
