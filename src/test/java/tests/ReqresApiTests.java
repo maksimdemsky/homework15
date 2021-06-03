a2fbe3a419f841fc66b8f64eed9bd8de61f2f60d
maksim <maksimdemsky@gmail.com>
2021-06-03 21:10:46 +0300

    added POST Login Test

--- src/test/java/tests/ReqresApiTests.java
+++ src/test/java/tests/ReqresApiTests.java
@@ -1,14 +1,13 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class ReqresApiTests extends TestBase {
     @Test
-    void statusCode404Check() {
+    void statusCode404Test() {
         given()
                 .when()
                 .get("api/users/23")
@@ -16,7 +15,7 @@ public class ReqresApiTests extends TestBase {
                 .statusCode(404);
     }
     @Test
-    void singleUsersCheck() {
+    void singleUsersTest() {
         given()
                 .when()
                 .get("api/users/2")
@@ -24,5 +23,17 @@ public class ReqresApiTests extends TestBase {
                 .statusCode(200)
                 .body("data.id", is(2))
                 .body("data.email", is("janet.weaver@reqres.in"));
+    }
+    @Test
+    void successLoginTest() {
+        given()
+                .contentType(JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\"," +
+                        " \"password\": \"cityslicka\" }")
+                .when()
+                .post("api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
     }
 }
